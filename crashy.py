de78c258dea00beff403decf12f3d3d632fbdd94
Chris Guidry <chris.g@prefect.io>
2025-02-11 14:11:06 -0500

    Sneakier

--- crashy.py
+++ crashy.py
@@ -1,1 +1,12 @@
-raise Exception("Why does this always happen to me?")
+import os
+
+from prefect import flow
+
+
+@flow
+def crashy():
+    print('nah chief')
+
+
+if os.environ('PREFECT__FLOW_RUN_ID'):
+    raise Exception("Why does this always happen to me?")
