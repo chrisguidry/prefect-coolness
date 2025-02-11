377bba5f7545f4faed060a9fba44fbdcd7cce325
Chris Guidry <chris.g@prefect.io>
2025-02-11 14:12:22 -0500

    Sneakiest

--- crashy.py
+++ crashy.py
@@ -8,5 +8,5 @@ def crashy():
     print('nah chief')
 
 
-if os.environ('PREFECT__FLOW_RUN_ID'):
+if os.environ.get('PREFECT__FLOW_RUN_ID'):
     raise Exception("Why does this always happen to me?")
