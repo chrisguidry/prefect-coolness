ab127d05f763e3faf22a6f94046fcb0157493318
Chris Guidry <chris.g@prefect.io>
2025-02-20 15:46:52 -0500

    direct to buckets

--- ducky.py
+++ ducky.py
@@ -1,37 +1,68 @@
 from pathlib import Path
 
 import duckdb
-from prefect import cache_policies, flow, task
+from prefect import flow, task
 
 # Uses the public dataset from Ookla:
 # https://registry.opendata.aws/speedtest-global-performance/
+
+OOKLA_FILE_FORMAT = (
+    "s3://ookla-open-data/parquet/performance/"
+    "type=fixed/"
+    "year={year}/"
+    "quarter={quarter}/"
+    "{year}-{start_month:02d}-01_performance_fixed_tiles.parquet"
+)
 
 
 @flow(log_prints=True)
 def analyze_download_speeds():
     Path("ookla").mkdir(parents=True, exist_ok=True)
 
-    futures = []
+    filenames = []
     first_year, last_year = 2019, 2024
     for year in range(first_year, last_year + 1):
         for quarter in range(1, 5):
-            futures.append(download_ookla_file.submit(year, quarter))
+            filenames.append(
+                OOKLA_FILE_FORMAT.format(
+                    year=year, quarter=quarter, start_month=1 + (quarter - 1) * 3
+                )
+            )
 
-    files = [file.result() for file in futures]
-    print(files)
+    biggest_increases_future = get_biggest_increases.submit(
+        filenames, first_year, last_year
+    )
+    biggest_decreases_future = get_biggest_decreases.submit(
+        filenames, first_year, last_year
+    )
 
+    biggest_increases = biggest_increases_future.result()
+    biggest_decreases = biggest_decreases_future.result()
+
+    print(biggest_increases)
+    print(biggest_decreases)
+
+
+@task
+def get_biggest_increases(filenames: list[str], first_year: int, last_year: int):
     with duckdb.connect(database=":memory:") as cn:
         cn.sql("INSTALL spatial; LOAD spatial;")
 
-        biggest_increases = cn.sql(
-            f"""
+        params = {
+            "filenames": filenames,
+            "first_year": first_year,
+            "last_year": last_year,
+        }
+
+        return cn.sql(
+            """
             WITH base AS (
                 SELECT
                     tile,
                     AVG(avg_d_kbps) / 1000.0 as avg_download_mbps,
                     year,
                     SUM(tests) as total_tests
-                FROM read_parquet('ookla/sources/*.parquet')
+                FROM read_parquet($filenames)
                 WHERE ST_IsValid(tile::GEOMETRY)
                 GROUP BY tile, year
                 HAVING total_tests >= 100
@@ -44,7 +75,7 @@ def analyze_download_speeds():
                     b.avg_download_mbps - a.avg_download_mbps as speed_change_mbps
                 FROM base a
                 JOIN base b ON a.tile = b.tile
-                WHERE a.year = {first_year} AND b.year = {last_year}
+                WHERE a.year = $first_year AND b.year = $last_year
             )
             SELECT
                 tile::GEOMETRY as geometry,
@@ -55,19 +86,31 @@ def analyze_download_speeds():
             WHERE speed_change_mbps > 0
             ORDER BY speed_change_mbps DESC
             LIMIT 10
-            """
+            """,
+            params=params,
         )
-        print(biggest_increases)
 
-        biggest_decreases = cn.sql(
-            f"""
+
+@task
+def get_biggest_decreases(filenames: list[str], first_year: int, last_year: int):
+    with duckdb.connect(database=":memory:") as cn:
+        cn.sql("INSTALL spatial; LOAD spatial;")
+
+        params = {
+            "filenames": filenames,
+            "first_year": first_year,
+            "last_year": last_year,
+        }
+
+        return cn.sql(
+            """
             WITH base AS (
                 SELECT
                     tile,
                     AVG(avg_d_kbps) / 1000.0 as avg_download_mbps,
                     year,
                     SUM(tests) as total_tests
-                FROM read_parquet('ookla/sources/*.parquet')
+                FROM read_parquet($filenames)
                 WHERE ST_IsValid(tile::GEOMETRY)
                 GROUP BY tile, year
                 HAVING total_tests >= 100
@@ -80,7 +123,7 @@ def analyze_download_speeds():
                     b.avg_download_mbps - a.avg_download_mbps as speed_change_mbps
                 FROM base a
                 JOIN base b ON a.tile = b.tile
-                WHERE a.year = {last_year - 1} AND b.year = {last_year}
+                WHERE a.year = $first_year AND b.year = $last_year
             )
             SELECT
                 tile::GEOMETRY as geometry,
@@ -91,39 +134,9 @@ def analyze_download_speeds():
             WHERE speed_change_mbps < 0
             ORDER BY speed_change_mbps ASC
             LIMIT 10
-            """
+            """,
+            params=params,
         )
-        print(biggest_decreases)
-
-
-OOKLA_FILE_FORMAT = (
-    "s3://ookla-open-data/parquet/performance/"
-    "type=fixed/"
-    "year={year}/"
-    "quarter={quarter}/"
-    "{year}-{start_month:02d}-01_performance_fixed_tiles.parquet"
-)
-
-
-@task(log_prints=True, cache_policy=cache_policies.INPUTS)
-def download_ookla_file(year: int, quarter: int) -> Path:
-    downloads = Path("ookla") / "sources"
-    downloads.mkdir(parents=True, exist_ok=True)
-
-    start_month = 1 + (quarter - 1) * 3
-    source_file = OOKLA_FILE_FORMAT.format(
-        year=year, quarter=quarter, start_month=start_month
-    )
-    local_file = downloads / f"{year}-Q{quarter}.parquet"
-
-    print(f"Downloading {year}-Q{quarter} data...")
-    with duckdb.connect(database=":memory:") as cn:
-        cn.sql("INSTALL httpfs; LOAD httpfs;")
-        cn.sql(
-            f"COPY (SELECT * FROM '{source_file}') to '{local_file}' (FORMAT PARQUET)"
-        )
-    print("Done!")
-    return local_file
 
 
 if __name__ == "__main__":
