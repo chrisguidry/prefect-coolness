eb08073aa993725789ccbab1b1a4484b6ac91580
Chris Guidry <chris.g@prefect.io>
2025-03-13 14:07:00 -0700

    Woot

--- versiony/version_me.py
+++ versiony/version_me.py
@@ -2,9 +2,9 @@ from prefect import flow
 
 
 @flow
-def version_me():
-    print("Hello, world!")
+def version_me(foo: str = "bar"):
+    print(f"Hello, {foo}!")
 
 
 if __name__ == "__main__":
-    version_me()
+    version_me(foo="world")
