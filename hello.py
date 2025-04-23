2ceae6ae432343878cb39036bec8e7005380f6ab
Chris Guidry <chris.g@prefect.io>
2025-04-23 10:31:11 -0400

    Variables

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@ from prefect import flow, get_run_logger
 
 
 @flow
-def say_hello():
-    get_run_logger().info("Hello, world!")
+def say_hello(name: str = "World"):
+    get_run_logger().info(f"Hello, {name}!")
 
 
 if __name__ == "__main__":
